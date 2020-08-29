06b9371c9f600e7cab22424e711cddd76c9f3794
Varun <31084854+Borumer@users.noreply.github.com>
2020-08-29 14:41:56 -0400

    Add all initial android files

---
+++ app/src/main/java/com/boruminc/borumjot/android/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.boruminc.borumjot.android;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
