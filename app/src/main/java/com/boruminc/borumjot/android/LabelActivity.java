130fe6c5f0786b483d545b791e48c69e63cac8e6
Varun <31084854+Borumer@users.noreply.github.com>
2020-09-08 22:20:52 -0400

    Implement Label Activity

--- app/src/main/java/com/boruminc/borumjot/android/LabelActivity.java
+++ app/src/main/java/com/boruminc/borumjot/android/LabelActivity.java
@@ -9,5 +9,6 @@ public class LabelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_label);
     }
 }
