cd070efe8ba7b1ec4b6c44e6dd673e07b281b6bd
Varun <31084854+Borumer@users.noreply.github.com>
2020-10-25 09:52:14 -0400

    Write AesEncryptionTest
    * Use Parameterized class to write Parameterized test for plaintextGetter_isCorrect() method. Checks for each set of values returned from AesEncryptionTest.input()
    * Create constructor - needed for parameterized test
    
    All tests pass

--- app/src/test/java/com/boruminc/borumjot/security/AesEncryptionTest.java
+++ app/src/test/java/com/boruminc/borumjot/security/AesEncryptionTest.java
@@ -2,15 +2,35 @@ package com.boruminc.borumjot.security;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Parameterized.class)
 public class AesEncryptionTest {
+    private String plaintext;
+
+    public AesEncryptionTest(String p) {
+        super();
+        plaintext = p;
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new String[][]{{"Hello world"}, {"Goodbye world"}, {"Bar"}, {"Foo"}});
+    }
 
     @Test
     public void instantiation_isPossible() {
         assertTrue(new AesEncryption("") instanceof AesEncryption);
+    }
+
+    @Test
+    public void plaintextGetter_isCorrect() {
+        assertEquals(plaintext, new AesEncryption(plaintext).getPlaintext());
     }
 }
