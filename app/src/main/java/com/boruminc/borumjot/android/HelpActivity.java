1a7d1ecb9396786bc16fc6f9fcc07e659b289868
Varun <31084854+Borumer@users.noreply.github.com>
2021-05-22 18:22:05 -0400

    Implement contact, remove forum
    * Implement contact button to give email clients to open email and have "TO" automatically be support@borumtech.com
    * Remove forum button because url isn't being fixed any time soon, add contact button

--- app/src/main/java/com/boruminc/borumjot/android/HelpActivity.java
+++ app/src/main/java/com/boruminc/borumjot/android/HelpActivity.java
@@ -2,10 +2,15 @@ package com.boruminc.borumjot.android;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelpActivity extends AppCompatActivity {
@@ -25,11 +30,24 @@ public class HelpActivity extends AppCompatActivity {
      * @param view The button that triggered this click event
      */
     public void onForumClick(View view) {
-        Intent borumIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.forum_link)));
-        startActivity(borumIntent);
+        Intent forumIntent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(getString(R.string.forum_link)));
+        startActivity(forumIntent);
     }
 
     public void onPrivacyPolicyClick(View view) {
         startActivity(new Intent(this, PrivacyPolicyActivity.class));
     }
+
+    public void onContactClick(View view) {
+        Uri uri = Uri.fromParts(
+                "mailto",
+                getString(R.string.support_email),
+                null
+        );
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri)
+                .putExtra(Intent.EXTRA_SUBJECT, "Feedback for Borum Jot");
+
+        startActivity(intent);
+    }
+
 }
