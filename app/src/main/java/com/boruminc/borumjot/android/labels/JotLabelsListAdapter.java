6b290359925b210112ba4576d614e6182496bffc
Varun <31084854+varunsingh87@users.noreply.github.com>
2021-09-08 12:25:55 -0400

    Implement add/remove label to note
    * Use JotLabelsListAdapter in JotLabelsList
    * Update onBindViewHolder() in JotLabelsListAdapter so that it updates the label list for a jotting with a request and invocation of a setter method correctly using the CompoundButton.OnCheckedChangedListener
    * Implement convertJSONToLabelBooleanMap() - Uses and returns ArrayList of JotLabel
    * Implement JotLabel - class that stores label and boolean representing whether the label is added to the jotting
    * Pass new argument jotType from NoteActivity to JotLabelsList
    * Update label-related requests to reflect API change

--- app/src/main/java/com/boruminc/borumjot/android/labels/JotLabelsListAdapter.java
+++ app/src/main/java/com/boruminc/borumjot/android/labels/JotLabelsListAdapter.java
@@ -1,22 +1,20 @@
 package com.boruminc.borumjot.android.labels;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.boruminc.borumjot.JotLabel;
 import com.boruminc.borumjot.Jotting;
-import com.boruminc.borumjot.Label;
 import com.boruminc.borumjot.android.R;
 import com.boruminc.borumjot.android.server.ApiResponseExecutor;
-import com.boruminc.borumjot.android.server.JSONToModel;
 import com.google.android.material.checkbox.MaterialCheckBox;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -24,10 +22,10 @@ import java.util.ArrayList;
 public class JotLabelsListAdapter extends RecyclerView.Adapter<JotLabelsListAdapter.ViewHolder> {
 
     private Context context;
-    private ArrayList<Label> labelData;
+    private ArrayList<JotLabel> labelData;
     private Jotting jotting;
 
-    JotLabelsListAdapter(Context ct, ArrayList<Label> d, Jotting j) {
+    JotLabelsListAdapter(Context ct, ArrayList<JotLabel> d, Jotting j) {
         context = ct;
         labelData = d;
         jotting = j;
@@ -42,36 +40,28 @@ public class JotLabelsListAdapter extends RecyclerView.Adapter<JotLabelsListAdap
 
     @Override
     public void onBindViewHolder(JotLabelsListAdapter.ViewHolder holder, int position) {
-        holder.name.setText(labelData.get(position).getName());
-        holder.name.setOnClickListener(v -> {
-            if (holder.editMode) {
-                MaterialCheckBox labelCheckbox = (MaterialCheckBox) v;
-                labelCheckbox.setChecked(!labelCheckbox.isChecked());
+        JotLabel labelBoolRelation = labelData.get(position);
+        holder.name.setChecked(labelBoolRelation.getBelongs());
+        holder.name.setText(labelBoolRelation.getLabel().getName());
 
-                if (labelCheckbox.isChecked())
-                    jotting.addLabel(labelData.get(position));
-                else
-                    jotting.removeLabel(labelData.get(position));
+        holder.name.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked)
+                jotting.addLabel(labelBoolRelation.getLabel());
+            else
+                jotting.removeLabel(labelBoolRelation.getLabel());
 
-                new UpdateJottingLabels(jotting, "note").runAsync(
-                        context.getSharedPreferences("user identification", Context.MODE_PRIVATE).getString("userApiKey", ""),
-                        new ApiResponseExecutor() {
-                            @Override
-                            public void onComplete(JSONObject result) {
-                                super.onComplete(result);
-                                try {
-                                    labelData = JSONToModel.convertJSONToLabels(result.getJSONArray("data"), true);
-                                } catch (JSONException e ) {
-                                    e.printStackTrace();
-                                }
+            new UpdateJottingLabels(jotting, "note").runAsync(
+                    context.getSharedPreferences("user identification", Context.MODE_PRIVATE).getString("userApiKey", ""),
+                    new ApiResponseExecutor() {
+                        @Override
+                        public void onComplete(JSONObject result) {
+                            super.onComplete(result);
+                            if (!ranOk()) {
+                                Toast.makeText(context, "An error occurred and the label could not be added to this jotting", Toast.LENGTH_SHORT).show();
                             }
                         }
-                );
-            } else {
-                Intent label = new Intent(context, LabelActivity.class);
-                label.putExtra("label", labelData.get(position));
-                context.startActivity(label);
-            }
+                    }
+            );
         });
     }
 
@@ -82,12 +72,10 @@ public class JotLabelsListAdapter extends RecyclerView.Adapter<JotLabelsListAdap
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         MaterialCheckBox name;
-        boolean editMode;
 
         ViewHolder(View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.label_name);
-            editMode = false;
         }
     }
 }
